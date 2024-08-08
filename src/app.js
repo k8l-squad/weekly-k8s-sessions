61a6e618781f7c5119ebf925e85c6db76f19696c
Thomas Whitaker <thomaswhitakerftw@gmail.com>
2024-08-08 17:24:27 -0400

    Add magic button code

--- src/app.js
+++ src/app.js
@@ -6,16 +6,12 @@ const path = __dirname + '/views/';
 const port = 8080;
 
 router.use(function (req,res,next) {
-    console.log('/' + req.method);
-    next();
+  console.log('/' + req.method);
+  next();
 });
   
-  router.get('/', function(req,res) {
-    res.sendFile(path + 'index.html');
-});
-  
-  router.get('/sharks', function(req,res) {
-    res.sendFile(path + 'sharks.html');
+router.get('/', function(req,res) {
+  res.sendFile(path + 'index.html');
 });
 
 app.use(express.static(path));
